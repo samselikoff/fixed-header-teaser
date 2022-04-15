3840ab6e47bb57c3e7062d280f2f4d84f59ab4e5
Sam Selikoff <sam.selikoff@gmail.com>
2022-04-15 16:05:45 -0400

    demo

---
+++ src/Article.js
@@ -0,0 +1,98 @@
+export default function Article() {
+  return (
+    <div className="space-y-4">
+      <p>
+        Totally blowing up on Twitter Jay Rosen Sloan monetization people
+        formerly known as the audience, copyright Journal Register
+        recontextualize fourth estate Fuego link economy Dan Fleckner the notion
+        of the public. Gamification going forward just across the wire stream
+        the notional night cops reporter in Des Moines. The Printing Press as an
+        Agent of Change Gutenberg newspaper iPad app, Kindle Single try PR
+        WikiLeaks Quora metered model right-sizing try PR.
+      </p>
+      <p>
+        Net neutrality syndicated Journal Register shoot a photo hashtag
+        curmudgeon 5% corruption analog thinking TBD eHow, community What Would
+        Google Do iPhone app aggregation fourth estate Snarkmarket linkbait I
+        saw it on Mediagazer, metered model Robin Sloan CPC explainer Buttry
+        Tumblr Android tags. Analytics Storify community 5 praise erasers & how
+        to avoid them got drudged writing curmudgeon libel lawyer, church of the
+        savvy Facebook Lucius Nieman David Foster Wallace mathewi RSS Josh
+        Marshall do what you do best and link to the rest, Nick Denton Pulse
+        totally blowing up on Twitter The Daily mthomps Jay Rosen.
+      </p>
+
+      <div className="py-6 lg:-mx-16 overflow-hidden">
+        <div className="relative w-full aspect-video">
+          <img src="/picture.jpg" alt="Space" />
+        </div>
+      </div>
+
+      <p>
+        Recontextualize This Week in Review content is king engagement Romenesko
+        RSS Wikipedia, data visualization advertising privacy discuss social
+        media natural-born blogger, the audience knows more than I do Alberto
+        Ibarguen The Work of Art in the Age of Mechanical Reproduction Nick
+        Denton masthead. I love the Weather & Opera section totally blowing up
+        on Twitter Tim Carmody masthead digital circulation strategy analytics
+        DocumentCloud got drudged, kitchen table of the future Frontline
+        newsroom cafe stupid commenters crowdsourcing open source we will make
+        them pay bot, perfect for starting a campfire Article Skimmer totally
+        blowing up on Twitter tabloid writing Gannett.
+      </p>
+
+      <p>
+        Production of innocence incent analog thinking data visualization HuffPo
+        retweet tablets Flipboard every dog loves food, media diet should isn’t
+        a business model libel lawyer fair use community Ushahidi Aron Pilhofer
+        WikiLeaks, Tumblr content farm David Cohn Patch serendipity engagement
+        CTR. Voice of San Diego masthead social media trolls reality-based
+        aggregation, HuffPo Encyclo recontextualize TBD, explainer newsroom cafe
+        free as in beer future of narrative.
+      </p>
+
+      <p>
+        Paul Steiger hackgate community perfect for starting a campfire The Work
+        of Art in the Age of Mechanical Reproduction SEO newspaper 5% corruption
+        meme Journal Register Neil Postman, inverted pyramid if the news is that
+        important, it’ll find me afternoon paper Article Skimmer West Seattle
+        Blog curmudgeon meme learnings curation, WikiLeaks fair use Dayton for
+        under $900 a day 5 praise erasers & how to avoid them CNN leaves it
+        there Bill Keller Groupon Josh Marshall production of innocence. future
+        of narrative incent fourth estate process vs. product DocumentCloud RSS
+        Instagram the power of the press belongs to the person who owns one
+        Instagram copyright, layoffs analog thinking DocumentCloud community
+        trolls curation attracting young readers crowdfunding abundance linking,
+        scoop filters going forward I saw it on Mediagazer innovation Politics &
+        Socks page nonprofit The Printing Press as an Agent of Change.
+      </p>
+
+      <p>
+        fourth estate community privacy just across the wire iPhone app
+        collaboration retweet DocumentCloud, linking +1 plagiarism experiment
+        inverted pyramid paidContent YouTube we will make them pay, Neil Postman
+        Josh Marshall Robin Sloan people formerly known as the audience Buttry
+        free as in beer. paidContent Knight News Challenge metered model morgue
+        layoffs The Weekender, scoop collaboration hyperlocal TechCrunch net
+        neutrality monetization, serendipity shoot a video if the news is that
+        important, it’ll find me Jeff Jarvis.
+      </p>
+
+      <p>
+        linkbait curation if the news is that important, it’ll find me
+        Sulzberger every dog loves food in the slot tweets Pictures of Goats
+        section curation, recontextualize gotta grok it before you rock it
+        Walter Cronkite died for your sins CPC Kindle Single Groupon Twitter
+        topples dictators syndicated discuss, curation Knight News Challenge
+        blog put the paper to bed he said she said trolls David Foster Wallace.
+        anonymity we need a Nate Silver Gutenberg Twitter topples dictators
+        linkbait newspaper strike if the news is that important, it’ll find me
+        cognitive surplus The Work of Art in the Age of Mechanical Reproduction,
+        Jeff Jarvis Clay Shirky lede I love the Weather & Opera section going
+        forward view from nowhere Romenesko people formerly known as the
+        audience, dying Steve Jobs Android media bias plagiarism every dog loves
+        food Gardening & War section.
+      </p>
+    </div>
+  );
+}
